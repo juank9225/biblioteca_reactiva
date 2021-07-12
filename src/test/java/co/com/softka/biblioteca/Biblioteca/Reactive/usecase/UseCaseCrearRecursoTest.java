3b67dcd283a87fde753c41e728afeb5beabe3eb0
juan carlos salcedo <juank9225@gmail.com>
2021-07-12 00:00:15 -0500

    Se crea el test para el caso de uso crear recurso

---
+++ src/test/java/co/com/softka/biblioteca/Biblioteca/Reactive/usecase/UseCaseCrearRecursoTest.java
@@ -0,0 +1,49 @@
+package co.com.softka.biblioteca.Biblioteca.Reactive.usecase;
+
+import co.com.softka.biblioteca.Biblioteca.Reactive.dto.RecursoDTO;
+import co.com.softka.biblioteca.Biblioteca.Reactive.modelo.Recurso;
+import co.com.softka.biblioteca.Biblioteca.Reactive.repositorio.RepositoryRecurso;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import reactor.core.publisher.Mono;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UseCaseCrearRecursoTest {
+
+
+    @SpyBean
+    private UseCaseCrearRecurso useCaseCrearRecurso;
+
+    @MockBean
+    private RepositoryRecurso repositoryRecurso;
+
+    @DisplayName("test crear recurso")
+    @Test
+    public void crearRecursoUseCaseTest() {
+
+        RecursoDTO recursoDTO = new RecursoDTO("cuento de terror","libro","11/07/2021",true,"22222");
+        Recurso recurso = new Recurso();
+        recurso.setNombre("cuento de terror");
+        recurso.setTipoRecurso("libro");
+        recurso.setFecha("11/07/2021");
+        recurso.setDisponible(true);
+        recurso.setAreaTematicaId("22222");
+
+        when(repositoryRecurso.save(Mockito.any(Recurso.class))).thenReturn(Mono.just(recurso));
+
+        var respuesta = useCaseCrearRecurso.apply(recursoDTO);
+
+        Assertions.assertEquals(respuesta.block().getNombre(),"cuento de terror");
+        Assertions.assertEquals(respuesta.block().getDisponible(),true);
+
+    }
+
+}
